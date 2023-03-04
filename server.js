6b7335de95e987858b65d7354d43798bf9426abf
Muhammadiy <muhammad.700@inbox.ru>
2023-03-04 18:26:45 +0500

    created paramethors for change hostname and port use argv and printed url with starting server

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const http = require('http');
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
+let hostname = 'localhost';
+let port = process.env.PORT || 3000;
+
+const args = process.argv.slice(2);
+
+if (args.length > 0) {
+    if (args[0].startsWith('--host')) {
+        hostname = args[0].split('=')[1];
+    }
+
+    if (args[1].startsWith('--port')) {
+        port = args[1].split('=')[1];
+    }
+}
 
 const server = http.createServer(app);
 
-server.listen(port, () => {
-    console.log(`Server running at port ` + port);
+server.listen(port, hostname, () => {
+    console.log(`Server running at port http://${hostname}:${port}`);
 });
